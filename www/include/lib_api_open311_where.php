043b72c2ffc2acd91fb705c9b3ff23a26fcae9a6
straup <straup@gmail.com>
2011-10-22 01:13:41 +0200

    where.getList; incidents.getInfo

--- www/include/lib_api_open311_where.php
+++ www/include/lib_api_open311_where.php
@@ -6,8 +6,20 @@
 
 	function api_open311_where_getList(){
 
-		$map = open311_incidents_search_where_map();
-		api_output_ok($map);
+		$map = open311_where_map();
+		$count = count($map);
+
+		# TO DO: real pagination...
+
+		$out = array(
+			'page' => 1,
+			'pages' => 1,
+			'per_page' => $count,
+			'total' => $count,
+			'terms' => $map,
+		);
+
+		api_output_ok($out);
 	}
 
 	##############################################################################
--- www/include/lib_open311_where.php
+++ www/include/lib_open311_where.php
@@ -4,9 +4,22 @@
 
 	function open311_where_map(){
 
+		# put me in a database? feels like overkill...
+
 		$map = array(
-			"bbox" => "",
+			array(
+				'prefix' => 'bbox',
+				'description' => 'Search for incident reports by geographic extent',
+				"example" => "bbox:37.788,-122.344,37.857,-122.256"
+			),
+			array(
+				'prefix' => 'zip',
+				'description' => 'Search for incident reports by zip code',
+				"example" => "zip:94110"
+			),
 		);
+
+		return $map;
 	}
 
 	##############################################################################
